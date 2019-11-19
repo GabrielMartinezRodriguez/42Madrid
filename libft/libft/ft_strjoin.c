7ea02b2b58ec3abf355a533c8925d871c2e942e3
Gabriel Martinez rodriguez <gmartine@c2r12s2.42madrid.com>
2019-11-19 20:46:53 +0100

    get_next_line

--- libft/libft/ft_strjoin.c
+++ libft/libft/ft_strjoin.c
@@ -6,36 +6,37 @@
 /*   By: gmartine <gmartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 11:42:44 by gmartine          #+#    #+#             */
-/*   Updated: 2019/11/16 17:28:22 by gmartine         ###   ########.fr       */
+/*   Updated: 2019/11/19 18:38:27 by gmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char			*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*aux;
-	size_t	i;
-	size_t	t_s2;
+	int		i;
+	int		t_s[2];
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	t_s2 = ft_strlen(s2);
-	aux = malloc(ft_strlen(s1) + t_s2 + 1);
+	t_s[0] = ft_strlen(s2);
+	t_s[1] = ft_strlen(s1);
+	aux = malloc(t_s[1] + t_s[0] + 1);
 	if (aux == NULL)
 		return (NULL);
 	i = 0;
-	while (i < ft_strlen(s1))
+	while (i < t_s[1])
 	{
 		aux[i] = s1[i];
 		i++;
 	}
 	i = 0;
-	while (i < t_s2)
+	while (i < t_s[0])
 	{
-		aux[i + ft_strlen(s1)] = s2[i];
+		aux[i + t_s[1]] = s2[i];
 		i++;
 	}
-	aux[i + ft_strlen(s1)] = '\0';
+	aux[i + t_s[1]] = '\0';
 	return (aux);
 }
